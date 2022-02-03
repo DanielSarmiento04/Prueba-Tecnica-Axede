7792c542f65fe7c321d4cf67bef921d3e9c76bf8
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2022-02-03 18:35:09 -0500

    v3.0

--- Proyecto.Persistencia/AppRepositorios/AppContext.cs
+++ Proyecto.Persistencia/AppRepositorios/AppContext.cs
@@ -7,6 +7,7 @@ namespace Proyecto.Persistencia
     public class AppContext: DbContext{
     
     public DbSet<Hotel>? Hoteles {get;set;}
+    public DbSet<Sede>? Sedes {get;set;}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             if(!optionsBuilder.IsConfigured){
                 optionsBuilder.UseSqlServer("Data Source = (local db)\\MSSQLLocalDB; Initial Catalog = Proyecto");
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Cosas A tener en cuenta
+1. Este repositorio hace parte de una prueba tecnica.
+2. Sofware: 
+ - Se uso La version de .net 6.0
+ - Sql server 2019 en su version local
+ - Git para el control de versiones.
+ - Visual Studio Code como entorno de desarrollo.
+
